27e7622d88bd96f2148788e55e3b1e0b117a052e
rafasilva9537 <rafasilva9537@gmail.com>
2024-07-23 16:35:56 -0300

    Site First Files

---
+++ recipes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# django namespace: create template folder with child folder, if not, html files can conflit with each other
+def home(request):
+    return render(request, "recipes/pages/home.html", status=200, context={'name': 'Lasagna'})
